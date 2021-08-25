aa27d8363bcf8429e910133f02883107541a1e7c
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-08-25 00:38:34 -0300

    reset default browser styles with 'styled-normalize'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Normalize } from 'styled-normalize'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
